a2ad022309c75a8c9feed09fdbfb2c115fb4e26a
Faraz Durrani <farazkhurram@msn.com>
2020-08-25 00:20:30 -0500

    more practice

--- src/com/algorithm/SquareMatrixMultiply.java
+++ src/com/algorithm/SquareMatrixMultiply.java
@@ -1,6 +1,6 @@
 package com.algorithm;
 
-public class SumOfSquareMatrix {
+public class SquareMatrixMultiply {
     public static void main(String[] args) {
 	int[][] A = new int[2][2];
 	A[0][0] = 1;
