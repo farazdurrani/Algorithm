ad9cee95ac6612ab1531f1ef220b8c5fbb6dbc1e
Faraz Durrani <farazkhurram@msn.com>
2020-09-05 11:31:37 -0500

    heapsort for Strings

---
+++ src/com/algorithm/heap/HeapSort5.java
@@ -0,0 +1,57 @@
+package com.algorithm.heap;
+
+import java.util.Arrays;
+
+public class HeapSort5 {
+    public static void main(String[] args) {
+	String A[] = { "d", "c", "b", "a", "h", "g", "f", "e", "m", "l", "k", "j", "i", "p", "o", "n" };
+//	String A[] = { "13", "-3", "-25", "20", "-3", "-16", "-23", "18", "20", "-7", "12", "-5", "-22", "15", "-4", "7" };
+	heapSort(A);
+	Arrays.stream(A).forEach(x -> System.out.print(x + " "));
+    }
+
+    private static void heapSort(String[] A) {
+	int heapSize = A.length - 1;
+	buildMaxHeap(A, heapSize);
+	for (int i = A.length - 1; i > 0; i--) {
+	    exchange(A, 0, i);
+	    heapSize -= 1;
+	    maxHeapify(A, 0, heapSize);
+	}
+    }
+
+    private static void buildMaxHeap(String[] a, int heapSize) {
+	for (int i = a.length / 2; i > -1; i--) {
+	    maxHeapify(a, i, heapSize);
+	}
+    }
+
+    private static void maxHeapify(String[] a, int i, int heapSize) {
+	int l = (2 * i) + 1;
+	int r = (2 * i) + 2;
+	int largest = i;
+	if (l <= heapSize && a[l].compareTo(a[largest]) > 0) { // n > e, p > f
+	    largest = l;
+	}
+	if (r <= heapSize && a[r].compareTo(a[largest]) > 0) { // o > p
+	    largest = r;
+	}
+	if (i != largest) {
+	    exchange(a, i, largest);
+	    maxHeapify(a, largest, heapSize);
+	}
+    }
+
+    private static void exchange(String[] a2, int a, int b) {
+	String temp = a2[a];
+	a2[a] = a2[b];
+	a2[b] = temp;
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+}
