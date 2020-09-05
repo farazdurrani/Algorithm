990b42227e096a486510a14e9c62383af1b34185
Faraz Durrani <farazkhurram@msn.com>
2020-09-04 20:25:42 -0500

    Added HeapSort

--- src/com/algorithm/heap/HeapSort4.java
+++ src/com/algorithm/heap/HeapSort4.java
@@ -1,28 +1,20 @@
 package com.algorithm.heap;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class HeapSort3 {
+public class HeapSort4 {
+
     public static void main(String[] args) {
 	int A[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
 	heapSort(A);
 	IntStream.of(A).forEach(x -> System.out.print(x + " "));
-	System.out.println();
-	int B[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
-	IntStream.of(B).forEach(x -> System.out.print(x + " "));
-	System.out.println();
-	List<Integer> C =IntStream.of(B).boxed().collect(Collectors.toList());
-	Collections.sort(C);
-	C.forEach(x -> System.out.print(x + " "));
     }
 
     private static void heapSort(int[] A) {
 	int heapSize = A.length - 1;
 	BUILD_MAX_HEAP(A, heapSize);
 	for (int i = A.length - 1; i > 0; i--) {
+	    //we will operate on 0
 	    exchange(A, 0, i);
 	    heapSize -= 1;
 	    MAX_HEAPIFY(A, 0, heapSize);
@@ -35,9 +27,9 @@ public class HeapSort3 {
 	}
     }
 
-    public static void MAX_HEAPIFY(int[] A, int i, int heapSize) {
-	int l = LEFT(i);
-	int r = RIGHT(i);
+    private static void MAX_HEAPIFY(int[] A, int i, int heapSize) {
+	int l = left(i);
+	int r = right(i);
 	int largest = i;
 	if (l <= heapSize && A[l] > A[largest]) {
 	    largest = l;
@@ -45,23 +37,39 @@ public class HeapSort3 {
 	if (r <= heapSize && A[r] > A[largest]) {
 	    largest = r;
 	}
-	if (largest != i) {
+	if (i != largest) {
 	    exchange(A, i, largest);
 	    MAX_HEAPIFY(A, largest, heapSize);
 	}
     }
 
-    private static int RIGHT(int i) {
+    private static void exchange(int[] arr, int a, int b) {
+	int temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
+    }
+
+    private static int right(int i) {
 	return (2 * i) + 2;
     }
 
-    private static int LEFT(int i) {
+    private static int left(int i) {
 	return (2 * i) + 1;
     }
 
-    private static void exchange(int[] A, int i, int largest) {
-	int temp = A[i];
-	A[i] = A[largest];
-	A[largest] = temp;
+    private static int parent(int i) {
+	return (i - 1) / 2;
+    }
+
+    private static int leftWhenOneBased(int i) {
+	return 2 * i;
+    }
+
+    private static int rightWhenOneBased(int i) {
+	return (2 * i) + 1;
+    }
+
+    private static int parentWhenOneBased(int i) {
+	return i / 2;
     }
 }
