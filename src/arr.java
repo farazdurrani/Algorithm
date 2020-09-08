7dde1dfddf6092a01363705fc153a626b9bdd0c6
Faraz Durrani <farazkhurram@msn.com>
2020-09-07 20:42:05 -0500

    Maximum Sub Array

--- src/arr.java
+++ src/arr.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
+
 public class arr {
-    int a[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+    public static void main(String[] args) {
+	int a[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+	Arrays.stream(a).forEach(x -> System.out.printf("%4d", x));
+	System.out.println();
+    }
 }
