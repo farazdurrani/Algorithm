25398d1e2dacbd83accc27b1cf6f39d7e817b381
Faraz Durrani <farazkhurram@msn.com>
2020-09-14 06:58:56 -0500

    Priority Queue via max-heap and HeapSort added. Max Sub Array BF and DaC added

--- src/com/algorithm/heap/PriorityQueueMaxHeapifyHeapSort6.java
+++ src/com/algorithm/heap/PriorityQueueMaxHeapifyHeapSort6.java
@@ -6,8 +6,8 @@ public class PriorityQueueMaxHeapifyHeapSort6 {
     private static int heapSize;
 
     public static void main(String[] args) {
-	int A[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
-//	int A[] = { 13, -3, -25, 20, 7, 99, 12 };
+//	int A[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+	int A[] = { 13, -3, -25, 20, 7, 99, 12 };
 	heapSort(A);
 	print(A, "After sort");
 	heapSize = A.length - 1;
