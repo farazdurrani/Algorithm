673aaf123c055aa9d665404bfc070ad375d229e8
Faraz Durrani <farazkhurram@msn.com>
2020-09-27 05:35:25 -0500

    FarazSort2 where min is put at the beginning of an array and max at the end, then the next selection-items shrink.

---
+++ src/com/algorithm/sort/FarazSort2.java
@@ -0,0 +1,48 @@
+package com.algorithm.sort;
+
+import java.util.Arrays;
+
+public class FarazSort2 {
+    public static void main(String[] args) {
+
+	int arr[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+	Arrays.stream(arr).forEach(x -> System.out.print(x + " "));
+	System.out.println();
+
+	int heapSize = arr.length - 1;
+
+	for (int i = 0; i < heapSize; i++, heapSize--) {
+	    int[] minMaxIndex = findMinMaxIndex(arr, i, heapSize);
+	    swap(arr, i, minMaxIndex[0]);
+	    swap(arr, heapSize, minMaxIndex[1]);
+	}
+
+	Arrays.stream(arr).forEach(x -> System.out.printf("%4d", x));
+	System.out.println();
+
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+	int temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
+    }
+
+    private static int[] findMinMaxIndex(int[] a, int j, int heapSize) {
+	int max = Integer.MIN_VALUE;
+	int maxIndex = 0;
+	int min = Integer.MAX_VALUE;
+	int minIndex = 0;
+	for (int i = j; i <= heapSize; i++) {
+	    if (a[i] >= max) {
+		max = a[i];
+		maxIndex = i;
+	    }
+	    if (min >= a[i]) {
+		min = a[i];
+		minIndex = i;
+	    }
+	}
+	return new int[] { minIndex, maxIndex };
+    }
+}
