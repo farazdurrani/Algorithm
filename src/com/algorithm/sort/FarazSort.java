42bf2ea28c9bf7f2ea145450bca9bd3739d55b3d
Faraz Durrani <farazkhurram@msn.com>
2020-09-27 02:30:49 -0500

    My own sorting algorithm!

---
+++ src/com/algorithm/sort/FarazSort.java
@@ -0,0 +1,46 @@
+package com.algorithm.sort;
+
+import java.util.Arrays;
+
+public class FarazSort {
+    public static void main(String[] args) {
+
+	int arr[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+	Arrays.stream(arr).forEach(x -> System.out.printf("%4d", x));
+	System.out.println();
+
+	int heapSize = arr.length - 1;
+
+	for (int i = 0; i < arr.length; i++) {
+	    int maxIndex = findMaxIndex(arr, heapSize);
+	    swap(arr, heapSize, maxIndex);
+	    heapSize--;
+	    if (heapSize < 0) {
+		System.out.println("Finished Sorted");
+		i = arr.length; // or break
+	    }
+	}
+
+	Arrays.stream(arr).forEach(x -> System.out.printf("%4d", x));
+	System.out.println();
+
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+	int temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
+    }
+
+    private static int findMaxIndex(int[] a, int heapSize) {
+	int max = Integer.MIN_VALUE;
+	int index = 0;
+	for (int i = 0; i <= heapSize; i++) {
+	    if (a[i] > max) {
+		max = a[i];
+		index = i;
+	    }
+	}
+	return index;
+    }
+}
