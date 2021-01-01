7ae367bf09a258c8e7521b7222aad0ecdceace44
Faraz Durrani <farazkhurram@msn.com>
2021-01-01 14:34:33 -0600

    Insertion and MergeSort

--- src/com/algorithm/sort/insertion/InsertionSort13.java
+++ src/com/algorithm/sort/insertion/InsertionSort13.java
@@ -3,16 +3,16 @@ package com.algorithm.sort.insertion;
 import java.util.Arrays;
 
 public class InsertionSort13 {
-private static int arr[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
-    
+    private static int arr[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+
     private static void print(int[] arr) {
 	Arrays.stream(arr).forEach(x -> System.out.print(x + " "));
-	System.out.println();    
+	System.out.println();
     }
-    
+
     public static void main(String[] args) {
 	print(arr);
-	for(int i = 1; i < arr.length; i++) {
+	for (int i = 1; i < arr.length; i++) {
 	    int key = arr[i];
 	    int j = i - 1;
 	    while (j > -1 && arr[j] > key) {
