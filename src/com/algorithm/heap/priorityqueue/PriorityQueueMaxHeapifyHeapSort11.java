b7abad28eb428e496de619d70849067a4986862c
Faraz Durrani <farazkhurram@msn.com>
2021-01-21 07:29:05 -0600

    more prac

---
+++ src/com/algorithm/heap/priorityqueue/PriorityQueueMaxHeapifyHeapSort11.java
@@ -0,0 +1,32 @@
+package com.algorithm.heap.priorityqueue;
+
+import java.util.stream.IntStream;
+//Notes:
+//maintainHeapProperty:
+//	This function is called on an element at i that might be violating max-heap property.
+//	Meaning, element at A[i] might be smaller than left or right child.
+
+//buildMaxHeap:
+//	Build max heap in a bottom up manner by calling maintainHeapProperty. But instead of starting from A.length - 1, 
+//	start from mid till the first element. Because children will be traversed by maintainHeapProperty.
+
+//heapSort:
+//	first, buildMaxHeap. Now, the largest value is sitting in the first position. 
+//	second, start the for loop from the bottom till the 2nd element in the array, 
+//		swap the first element with the heapSize, 
+//		shrink the heapSize, 
+//		call maintainHeapProperty on the first element (because first element might be breaching max-heap-property).
+
+public class PriorityQueueMaxHeapifyHeapSort11 {
+    static int heapSize;
+//	static int a[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+    static int A[] = { 13, -3, -25, 20, 7, 99, 12 };
+
+    private static void print(int[] a, String msg) {
+	System.out.println(msg);
+	IntStream.of(a).forEach(x -> System.out.printf("%4d", x));
+	System.out.println();
+    }
+    
+    
+}
