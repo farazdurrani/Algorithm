698854fe0ecb228c41cf0220d570e7cace7ed2ba
Faraz Durrani <farazkhurram@msn.com>
2021-02-08 00:31:49 -0600

    reverse linkedlist

---
+++ src/com/algorithm/dynamiclist15/Queue.java
@@ -0,0 +1,15 @@
+package com.algorithm.dynamiclist15;
+
+public class Queue<E> {
+
+    public void enqueue(String string) {
+    }
+
+    public void print() {
+    }
+
+    public String dequeue() {
+	return null;
+    }
+
+}
