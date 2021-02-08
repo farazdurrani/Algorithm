b25d6a409de79616221b02a15621f6f5d5cb0c3b
Faraz Durrani <farazkhurram@msn.com>
2021-02-08 00:31:49 -0600

    reverse linkedlist

---
+++ src/com/algorithm/dynamiclist15/Stack.java
@@ -0,0 +1,15 @@
+package com.algorithm.dynamiclist15;
+
+public class Stack<E> {
+
+    public void push(long l) {
+    }
+
+    public void print() {
+    }
+
+    public String pop() {
+	return null;
+    }
+
+}
