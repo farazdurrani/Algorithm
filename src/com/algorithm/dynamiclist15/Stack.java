dea9e53194a7cb7b377bcf5b8c8fcdf4b87a9154
Faraz Durrani <farazkhurram@msn.com>
2021-02-08 13:56:03 -0600

    reverse a doubly-linked Linked List

--- src/com/algorithm/dynamiclist15/Stack.java
+++ src/com/algorithm/dynamiclist15/Stack.java
@@ -1,15 +1,19 @@
 package com.algorithm.dynamiclist15;
 
-public class Stack<E> {
+public class Stack<E extends Comparable<E>> {
+    
+    private LinkedList<E> list = new LinkedList<>();
 
-    public void push(long l) {
+    public void push(E data) {
+	list.insertAtBack(data);
     }
 
     public void print() {
+	list.print();
     }
 
-    public String pop() {
-	return null;
+    public E pop() {
+	return list.removeFromBack();
     }
 
 }
