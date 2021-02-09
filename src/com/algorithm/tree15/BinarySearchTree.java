bab631098022673623f805acefe0c4fb806770f6
Faraz Durrani <farazkhurram@msn.com>
2021-02-08 18:00:59 -0600

    ground work for future BST work

---
+++ src/com/algorithm/tree15/BinarySearchTree.java
@@ -0,0 +1,50 @@
+package com.algorithm.tree15;
+
+public class BinarySearchTree<E extends Comparable<E>> {
+
+    public void insert(E data) {
+    }
+
+    public void levelOrderTraversalIterative() {
+    }
+
+    public void inorderTraversalResursive() {
+    }
+
+    public void levelOrderTraversalQueue() {
+    }
+
+    public E getRoot() {
+	return null;
+    }
+
+    public boolean contains(E data) {
+	return false;
+    }
+
+    public void remove(E data) {
+    }
+
+    public int height() {
+	return -1;
+    }
+
+    public void invertTree() {
+    }
+
+    public void preorderTraversalRecursive() {
+    }
+
+    public void postorderTraversalRecursive() {
+    }
+
+    public void inorderTraversalInterative() {
+    }
+
+    public void preorderTraversalIterative() {
+    }
+
+    public void postorderTraversalIterative() {
+    }
+
+}
