715158d9c0758d8ef47009b1d73c1c5fe7a71f6f
= <faraz.uic@gmail.com>
2021-05-03 00:21:33 -0500

    CutRod dynamic programming revisited

--- src/com/algorithm/dynamic/programming/cutrod/CutRod4.java
+++ src/com/algorithm/dynamic/programming/cutrod/CutRod4.java
@@ -2,7 +2,7 @@ package com.algorithm.dynamic.programming.cutrod;
 
 import java.util.Arrays;
 
-public class CutRod3 {
+public class CutRod4 {
 
 	public static void main(String[] args) {
 		int[] p = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 23, 30 };
@@ -22,11 +22,36 @@ public class CutRod3 {
 
 	}
 
-	private static void printCutRodSolution(int[] p, int i) {
+	private static void printCutRodSolution(int[] p, int n) {
+		int[][]rs = bottomUpCutRodModified(p, n);
+		
+		while(n > 0) {
+			System.out.print(rs[1][n] + " ");
+			n = n - rs[1][n];
+		}
+		System.out.println();
+	}
+
+	private static int[][] bottomUpCutRodModified(int []p, int n) {
+		int r[] = new int[n+1];
+		int s[] = new int[n+1];
+		
+		r[0] = 0;
+		for(int j = 1; j <= n; j++) {
+			int q = Integer.MIN_VALUE;
+			for(int i = 1; i <=j ;i++) {
+				if(q < p[i] + r[j-i]) {
+					q = p[i] + r[j-i];
+					s[j] = i;
+				}
+			}
+			r[j] = q;
+		}
+		return new int[][] {r,s};
 	}
 
 	private static int bottomUpCutRod(int[] p, int n) {
-		int r [] = new int[n+1];
+		int r[] = new int[n+1];
 		r[0] = 0;
 		for(int j = 1; j <= n; j++) {
 			int q = Integer.MIN_VALUE;
@@ -35,35 +60,34 @@ public class CutRod3 {
 			}
 			r[j] = q;
 		}
-			
 		return r[n];
 	}
 
 	private static int memoizedCutRod(int[] p, int n) {
-
-		int[] r = new int[n + 1];
+		int [] r = new int[n+1];
 		Arrays.fill(r, Integer.MIN_VALUE);
 		return memoizedCutRodAuxillary(p, n, r);
 	}
 
 	private static int memoizedCutRodAuxillary(int[] p, int n, int[] r) {
-		if (r[n] >= 0) {
+		if(r[n] >= 0) {
 			return r[n];
 		}
 		int q;
-		if (n == 0) {
+		if(n == 0) {
 			q = 0;
 		} else {
 			q = Integer.MIN_VALUE;
 		}
-		for (int i = 1; i <= n; i++) {
-			q = Math.max(q, p[i] + memoizedCutRodAuxillary(p, n - i, r));
+		for(int i = 1; i <= n; i++) {
+			q = Math.max(q, p[i] +  memoizedCutRodAuxillary(p, n-i, r));
 		}
 		r[n] = q;
 		return q;
 	}
 
 	private static int cutRod(int[] p, int n) {
+
 		if (n == 0) {
 			return 0;
 		}
@@ -71,6 +95,7 @@ public class CutRod3 {
 		for (int i = 1; i <= n; i++) {
 			q = Math.max(q, p[i] + cutRod(p, n - i));
 		}
+
 		return q;
 	}
 
