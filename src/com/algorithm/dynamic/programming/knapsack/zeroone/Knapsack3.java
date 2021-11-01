7d1b14abf92bff13466b559e1ebb7c85bdebe28a
Faraz Durrani-lubuntu <farazkhurram@msn.com>
2021-10-31 22:54:28 -0500

    Added Binary (0-1) Knapsack Problem

--- src/com/algorithm/dynamic/programming/knapsack/zeroone/Knapsack3.java
+++ src/com/algorithm/dynamic/programming/knapsack/zeroone/Knapsack3.java
@@ -1,15 +1,23 @@
 package com.algorithm.dynamic.programming.knapsack.zeroone;
 
-//https://medium.com/@fabianterh/how-to-solve-the-knapsack-problem-with-dynamic-programming-eb88c706d3cf
+/**
+ * Get the pseudo-code from https://en.wikipedia.org/wiki/Knapsack_problem#0-1_knapsack_problem
+ * Got the understanding of 'i' and 'j' and why we are doing '(j - w[i]) + v[i]' from:
+ * https://medium.com/@fabianterh/how-to-solve-the-knapsack-problem-with-dynamic-programming-eb88c706d3cf
+ * 
+ * @author faraz
+ *
+ */
 public class Knapsack3 {
 	public static void main(String[] args) {
 		int[] w = { 24, 10, 10, 7 };
 		int[] v = { 24, 18, 18, 10 };
 		int W = 25;
-
+		//ans 36
 //		int[] w = { 10, 40, 20 };
 //		int[] v = { 100, 280, 120 };
 //		int W = 60;
+		//ans 400
 
 		int n = w.length; // number of items
 
