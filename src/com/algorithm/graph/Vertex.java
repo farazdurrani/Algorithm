ce5eafc579248f85844dc7163141d094a5d4e03c
Faraz Durrani <farazkhurram@msn.com>
2021-11-25 08:20:39 -0600

    coded up Depth First Search

--- src/com/algorithm/graph/Vertex.java
+++ src/com/algorithm/graph/Vertex.java
@@ -4,11 +4,11 @@ package com.algorithm.graph;
 
 public class Vertex {
 
-	String label;
-	Color c;
-	Vertex p; //parent
-	int d; //discovery time;
-	int f; //found time
+	public String label;
+	public Color c;
+	public Vertex p; //parent
+	public int d; //discovery time;
+	public int f; //found time
 //	List<Vertex> adj; //adjencent vertices
 	
 	public Vertex(String label) {
@@ -19,11 +19,7 @@ public class Vertex {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((c == null) ? 0 : c.hashCode());
-		result = prime * result + d;
-		result = prime * result + f;
 		result = prime * result + ((label == null) ? 0 : label.hashCode());
-		result = prime * result + ((p == null) ? 0 : p.hashCode());
 		return result;
 	}
 
@@ -36,21 +32,10 @@ public class Vertex {
 		if (getClass() != obj.getClass())
 			return false;
 		Vertex other = (Vertex) obj;
-		if (c != other.c)
-			return false;
-		if (d != other.d)
-			return false;
-		if (f != other.f)
-			return false;
 		if (label == null) {
 			if (other.label != null)
 				return false;
 		} else if (!label.equals(other.label))
-			return false;
-		if (p == null) {
-			if (other.p != null)
-				return false;
-		} else if (!p.equals(other.p))
 			return false;
 		return true;
 	}
